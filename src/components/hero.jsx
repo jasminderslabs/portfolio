931c453d1a7f3b9683d3bb51283f68de4e5b0f40
“[Jasminder <pranav@sookshum-labs.com>
2026-01-07 11:41:40 +0530

    added tailwinds and motion package

---
+++ src/components/hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='w-full border bg-red-500 h-52'>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Hero
